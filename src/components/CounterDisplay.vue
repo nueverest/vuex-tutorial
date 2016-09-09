c8d279d36303cf761c308661b2300317d65e3ce3
Chad Nelson <nu.everest@gmail.com>
2016-09-09 10:27:04 -0700

    Implemented http://skyronic.com/2016/01/03/vuex-basics-tutorial/

---
+++ src/components/CounterDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+  Count is {{ count }}
+</template>
+
+<script>
+  import store from '../vuex-store'
+
+  export default {
+    computed: {
+      count () {
+        console.log(store.state.counter)
+        return store.state.counter
+      }
+    }
+  }
+</script>
+
+<style scoped></style>
---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click.prevent="activate">+1</button>
+  </div>
+</template>
+
+<script>
+  import store from '../vuex-store'
+
+  export default {
+    methods: {
+      activate () {
+        store.dispatch('INCREMENT')
+        console.log('+1 Pressed')
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
